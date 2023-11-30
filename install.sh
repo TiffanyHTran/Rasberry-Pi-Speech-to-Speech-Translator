793e467b16159b6448d2b8ad1b2ff1e6c6728ba7
Melissa Huynh <54555980+melissaaaah@users.noreply.github.com>
2023-11-29 23:28:35 -0800

    Update install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+# Update system
+sudo apt-get update
+
+# Install Python dependencies
+sudo apt install -y python3-pip
+pip3 install SpeechRecognition
+sudo apt-get install python3-pyaudio
+pip3 install googletrans==4.0.0-rc1
+pip3 install gTTS
